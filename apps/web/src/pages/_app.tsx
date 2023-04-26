16e7641e78a64bee7f4febf05a011492002ae4f8
th-release <cth.release@gmail.com>
2023-04-26 14:08:20 +0900

    feat: tokenCheck

--- apps/web/src/pages/_app.tsx
+++ apps/web/src/pages/_app.tsx
@@ -1,10 +1,14 @@
 import Head from "@/components/nextHead";
 import GlobalReducers from "@/modules/GlobalReducers";
 import { GlobalStyle } from "@/styles/globals";
+import { getCookie } from "@/utils/cookies";
+import axios from "axios";
 import { AppProps } from "next/app";
+import { useRouter } from "next/router";
 import { useEffect, useReducer } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
   const [event, update] = useReducer(GlobalReducers, {
     size: {
       width: 0, 
@@ -16,8 +20,24 @@ export default function App({ Component, pageProps }: AppProps) {
     update({ type: 'reSizeEvent', width: window.innerWidth, height: window.innerHeight })
   }
 
+  async function tokenCheck() {
+    const token = getCookie('GBSW_SESSION')
+    if (!router.asPath.includes('/callback')) {
+      if (!token) {
+        await axios(process.env.NEXT_PUBLIC_BASE_URL + '/api/account/getLogin', {
+          method: "GET"
+        }).then((res) => {
+          return router.push(res.data.data)
+        })
+      } else {
+        
+      }
+    }
+  }
+
   useEffect(() => {
     handleResize();
+    tokenCheck();
     window.addEventListener("resize", handleResize, false);
   }, [])
 
@@ -28,5 +48,5 @@ export default function App({ Component, pageProps }: AppProps) {
 
       <Component {...pageProps} size={event.size} />
     </>
-  )
+    )
 }

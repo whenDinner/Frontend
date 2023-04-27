7a3b81f6566ee6c21b4aee4404fea81d70bf7275
th-release <cth.release@gmail.com>
2023-04-27 10:28:34 +0900

    style: add fonts

--- apps/web/src/pages/_document.tsx
+++ apps/web/src/pages/_document.tsx
@@ -33,7 +33,12 @@ export default class _Document extends Document {
   render() {
     return (
       <Html>
-        <Head></Head>
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='' />
+          <link href="https://fonts.googleapis.com/css2?family=Sriracha&display=swap" rel="stylesheet" />
+          <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+        </Head>
         <body>
           <Main/>
           <NextScript/>

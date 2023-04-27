ae7d66584d495320e4b618b09517016241d7a988
th-release <cth.release@gmail.com>
2023-04-27 20:30:01 +0900

    style: update Banner

--- apps/web/src/components/Banner.tsx
+++ apps/web/src/components/Banner.tsx
@@ -1,12 +1,14 @@
+import { GlobalProps } from "@/utils/interface"
 import styled from "styled-components"
 
-export default function Banner() {
+export default function Banner({ size, tab }: GlobalProps) {
   return (
     <Body>
-      <Image src="/assets/gisuksa.jpeg" alt="Banner"></Image>
+      <Image src="/assets/gisuksa.jpeg" alt="Banner" className={tab === "tab on" ? "on" : "off"}></Image>
     </Body>
   )
 }
+
 const Body = styled.div`
   position: fixed;
   left: 0;
@@ -16,9 +18,22 @@ const Body = styled.div`
 `
 
 const Image = styled.img`
+  transition: all .35s;
   display: flex;
   aspect-ratio: 16/9;
   object-fit: cover;
   width: 100%;
-  height: 100vh;
+  height: 100%;
+
+  @media (max-width: 1040px) {
+    &.on {
+      width: 175%;
+    }
+  }
+
+  @media (min-width: 1040px) {
+    &.on {
+      width: 123%;
+    }
+  }
 `

e061a05a8d6da3c47e9c0b2cc321c53218ca1aa3
cth.release <84012697+th-release@users.noreply.github.com>
2023-05-23 16:30:17 +0900

    Delete apps/web/utils directory

--- apps/web/utils/interfaces.ts
+++
@@ -1,8 +0,0 @@
-import { Dispatch, SetStateAction } from "react"
-
-export type TabType = "tab-normal" | "tab-on" | "tab-off"
-
-export interface TabProps {
-  tab: TabType,
-  update: Dispatch<SetStateAction<TabType>>
-}

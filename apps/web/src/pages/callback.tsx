8e04aebc8cf1bf97a7cf89a12f165e6b686e00b4
cth.release <84012697+th-release@users.noreply.github.com>
2023-07-11 19:12:54 +0900

    Update callback.tsx

--- apps/web/src/pages/callback.tsx
+++ apps/web/src/pages/callback.tsx
@@ -3,24 +3,26 @@ import Container from "@/components/container";
 import { setCookie } from "@/utils/cookies";
 import axios from "axios";
 import { useRouter } from "next/router"
-import useSWR from 'swr'
-
-const callbackFetcher = async (path: string) => {
-  await axios(path, {
-    method: 'GET'
-  })
-  .then((res) => {
-    setCookie('whenDinner-session', res.data.token, { path: '/' })
-    window.location.href = '/auth/user'
-  })
-  .catch((err) => {
-    console.error(err)
-  })
-}
+import { useEffect } from "react";
 
 export default function Callback() {
   const router = useRouter();
-  const { data } = useSWR(`${process.env.NEXT_PUBLIC_BASE_URL}/api/account/callback?id_token=${router.query.id_token}&state=${router.query.state}`, callbackFetcher)
+
+  async function callBack() {
+    await axios(`${process.env.NEXT_PUBLIC_BASE_URL}/api/account/callback?id_token=${router.query.id_token}&state=${router.query.state}`, {
+      method: 'GET'
+    }).then((res) => {
+      setCookie('whenDinner-session', res.data.token, { path: '/' })
+      window.location.href = '/auth/user'
+    })
+    .catch((err) => {
+      console.error(err)
+    })
+  }
+
+  useEffect(() => {
+    callBack()
+  }, [router.isReady])
 
   return (
     <Container>

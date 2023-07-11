5c37ebf9949fcd164303de3008decaa01aecc68e
cth.release <84012697+th-release@users.noreply.github.com>
2023-07-11 09:52:45 +0900

    Update [name].tsx

--- apps/web/src/pages/qr/search/[name].tsx
+++ apps/web/src/pages/qr/search/[name].tsx
@@ -5,7 +5,6 @@ import { getCookie } from "@/utils/cookies";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import Pagination from "@/components/Pagination";
-import styled from "styled-components";
 import Link from "next/link";
 import { QuickResponse } from "@/utils/interfaces";
 import { useRouter } from "next/router";
@@ -38,7 +37,7 @@ export default function Home() {
 
   useEffect(() => {
     getQuickResponse();
-  }, [currentpage])
+  }, [currentpage, getQuickResponse])
 
   return (
     <Container>
@@ -53,7 +52,7 @@ export default function Home() {
       </Card>
       
       {items.map((value: QuickResponse, index: number) => (
-        <Link href={`/qr/${value.uuid}`}>
+        <Link href={`/qr/${value.uuid}`} key={index}>
           <Card key={index} title={value.name}>
             <li>uuid: {value.uuid}</li><br/>
             <Button type="button" text={value.action} color="#5d87ff" onClick={undefined} fontColor="#fff"></Button>
